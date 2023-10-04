f0d260735d5d80544d4c84d2eaeb7755560560f1
Janek7171 <st.randed68@gmail.com>
2023-10-04 21:32:26 +0200

    refactored code

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = ({ onSearch }) => {
       onKeyUp={handleKeyUp}
       className="bg-slate-200 border-4 focus:outline-none border-slate-300 rounded-2xl p-1 mx-10 mt-10 h-max w-max "
       type="text"
-      placeholder="Gotta search them all!"
+      placeholder="Gotta search for them all!"
     ></input>
   );
 };
